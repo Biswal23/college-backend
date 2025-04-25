164b279a135d0455ab1cb51637ca9020877e0c90
Biswal23 <133970942+Biswal23@users.noreply.github.com>
2025-04-25 08:21:26 +0530

    Update database.py
    it

--- database.py
+++ database.py
@@ -1,15 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import text
 
-# Database URL for SQLite
 DATABASE_URL = "sqlite:///./college.db"
-
-# Create the SQLAlchemy engine
-engine = create_engine("sqlite:///./college.db")
-
-# Create a configured "Session" class
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})  # Required for SQLite
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Create a base class for your models
 Base = declarative_base()
