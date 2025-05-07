5cdb109574e0ce99f482f89d3a761f25d56dc03b
Biswal23 <133970942+Biswal23@users.noreply.github.com>
2025-05-07 09:04:13 +0530

    Update models.py

--- models.py
+++ models.py
@@ -3,18 +3,21 @@ from database import Base
 
 class College(Base):
     __tablename__ = "colleges"
+
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
-    state = Column(String)
-    location = Column(String)
-    course_level = Column(String)
-    branch = Column(String)
-    fees = Column(Float)
-    cutoff = Column(Float)
+    state = Column(String, nullable=True)
+    location = Column(String, nullable=True)
+    course_level = Column(String, nullable=True)
+    branch = Column(String, nullable=True)
+    fees = Column(Float, nullable=True)
+    cutoff_min = Column(Float, nullable=True)
+    cutoff_max = Column(Float, nullable=True)
 
 class Review(Base):
     __tablename__ = "reviews"
+
     id = Column(Integer, primary_key=True, index=True)
     college_name = Column(String, index=True)
-    review_text = Column(String)
-    rating = Column(Float)
+    review_text = Column(String, nullable=True)
+    rating = Column(Float, nullable=True)
